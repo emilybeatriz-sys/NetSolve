d347e2a64d894518e8c27a9853b7a26a72dc18e8
emilybeatriz-sys <emilybeatriz06@gmail.com>
2026-04-28 11:38:34 +0000

    Aula 09 - if/else e novos botões

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-// let — pode mudar de valor (perfeito para o contador)
 let contador = 0;
 const btn = document.getElementById("btn-interativo");
 const msg = document.getElementById("mensagem");
 btn.onclick = function() {
   const nome = prompt('Qual é o seu nome?');
-  contador = contador + 1;  // incrementa o contador
-  // Number (contador) + String (nome) na mesma mensagem
-  msg.innerHTML = "Ola, " + nome + "! Voce clicou " + contador + " vez(es).";
+  contador = contador + 1;  
+  if (contador < 5) {
+    msg.innerHTML = "Olá, " + nome + "! Você clicou " + contador + " vez(es). Continue!";
+  }else if (contador < 10) {
+    msg.innerHTML = "Olá, " + nome + "! Você clicou " + contador + " vez(es). Quase lá!";
+  }else {
+    msg.innerHTML = "Limite de 10 cliques atingido, " + nome + "!";
+  }
 };
+function zerar() {
+  contador = 0;
+  msg.innerHTML = "Contador zerado! Clique novamente para recomeçar.";
+}
+
+document.getElementById("btn-zerar")
+  .addEventListener("click", zerar);
+
 
 
 
